89445473331b345c5b1caac5ba1422bd2e58cee1
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-01 13:06:16 +0200

    insert data into related tables works

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-from parse_json import JSON_NAME, get_gender
+from parse_json import JSON_NAME, get_not_nested_column_data, get_title_nested_column_data
 
 Base = declarative_base()
 
@@ -11,10 +11,23 @@ class Person(Base):
     __tablename__ = 'persons'
 
     id = Column('id', Integer, primary_key=True)
+    name = relationship("Name", uselist=False, back_populates="person")
     gender = Column(String)
 
     def __repr__(self):
         return f'(id:{self.id}, gender:{self.name}'
+
+
+class Name(Base):
+    __tablename__ = 'names'
+
+    id = Column('id', Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('persons.id'))
+    person = relationship("Person", back_populates="name")
+    title = Column(String)
+
+    def __repr__(self):
+        return f'(id:{self.id}, title:{self.title}'
 
 
 engine = create_engine('sqlite:///persons.db', echo=True)
@@ -24,8 +37,8 @@ Session = sessionmaker(bind=engine)
 session = Session()
 person = Person()
 
-person.gender = get_gender(JSON_NAME)
-
+person.gender = get_not_nested_column_data(JSON_NAME, "gender")
+person.name = Name(title=get_title_nested_column_data(JSON_NAME))
 session.add(person)
 session.commit()
 session.close()
