2111d26543ae90c6ed056113dda58a0b40d95e72
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-01 11:34:34 +0200

    change function to get data to be more flexible

--- parse_json.py
+++ parse_json.py
@@ -3,9 +3,10 @@ import json
 JSON_NAME = "persons.json"
 
 
-def get_gender(json_name):
+def get_not_nested_column_data(json_name, column_name):
+    column_name = str(column_name)
     json_file = open(json_name, 'r')
     json_data = json_file.read()
     obj = json.loads(json_data)
-    obj = obj['results'][0]["gender"]
+    obj = obj['results'][0][column_name]
     return obj
