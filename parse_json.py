b6ac0c185d13455287b419720a57e3955bfcf435
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-03 09:50:23 +0200

    fix return obj

--- parse_json.py
+++ parse_json.py
@@ -11,9 +11,19 @@ def get_not_nested_column_data(json_name, column_name):
     obj = obj['results'][0][column_name]
     return obj
 
+
 def get_title_nested_column_data(json_name):
     json_file = open(json_name, 'r')
     json_data = json_file.read()
     obj = json.loads(json_data)
     obj = obj['results'][0]['name']['title']
+    return obj
+
+
+def get_street_name_or_number(json_name, column_name):
+    column_name = str(column_name)
+    json_file = open(json_name, 'r')
+    json_data = json_file.read()
+    obj = json.loads(json_data)
+    obj = obj['results'][0]['location']['street'][column_name]
     return obj
