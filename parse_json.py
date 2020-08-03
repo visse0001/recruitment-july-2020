67d1fa8a10dcd19bd158c4f66af65299110f832b
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-03 22:50:09 +0200

    create remove_special_characters_from_string function

--- parse_json.py
+++ parse_json.py
@@ -2,6 +2,7 @@ import json
 
 JSON_NAME = "persons.json"
 
+
 def get_json_dict(json_name):
     json_file = open(json_name, 'r')
     json_data = json_file.read()
@@ -25,3 +26,11 @@ def get_triple_nested_table_data(index: int, first_table: str, second_table: str
     result = get_json_dict(JSON_NAME)
     obj = result['results'][index][first_table][second_table][third_table]
     return obj
+
+
+def remove_special_characters_from_string(a_string: str):
+    alpha_numeric = ""
+    for character in a_string:
+        if character.isalnum():
+            alpha_numeric += character
+    return alpha_numeric
