bf11d643f96d2598ec899b679640cc6236342fc8
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-03 22:37:37 +0200

    refactor parse json functions

--- parse_json.py
+++ parse_json.py
@@ -2,26 +2,26 @@ import json
 
 JSON_NAME = "persons.json"
 
-
-def get_not_nested_table_data(json_name: str, index: int, table_name: str):
+def get_json_dict(json_name):
     json_file = open(json_name, 'r')
     json_data = json_file.read()
     obj = json.loads(json_data)
-    obj = obj['results'][index][table_name]
+    return obj
+
+
+def get_not_nested_table_data(json_name: str, index: int, table_name: str):
+    result = get_json_dict(JSON_NAME)
+    obj = result['results'][index][table_name]
     return obj
 
 
 def get_double_nested_table_data(json_name: str, index: int, first_table: str, second_table: str):
-    json_file = open(json_name, 'r')
-    json_data = json_file.read()
-    obj = json.loads(json_data)
-    obj = obj['results'][index][first_table][second_table]
+    result = get_json_dict(JSON_NAME)
+    obj = result['results'][index][first_table][second_table]
     return obj
 
 
 def get_triple_nested_table_data(json_name: str, index: int, first_table: str, second_table: str, third_table: str):
-    json_file = open(json_name, 'r')
-    json_data = json_file.read()
-    obj = json.loads(json_data)
-    obj = obj['results'][index][first_table][second_table][third_table]
+    result = get_json_dict(JSON_NAME)
+    obj = result['results'][index][first_table][second_table][third_table]
     return obj
