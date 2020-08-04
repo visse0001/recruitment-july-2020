34f49cbafb870fab8a3d5460a412962fc6d01354
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-04 08:06:26 +0200

    create Dob table and INSERT data

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Unicode, ForeignK
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-from parse_json import JSON_NAME, get_not_nested_table_data, get_double_nested_table_data, get_triple_nested_table_data, remove_special_characters_from_string,get_days_until_birthday
+from parse_json import JSON_NAME, get_not_nested_table_data, get_double_nested_table_data, get_triple_nested_table_data, remove_special_characters_from_string, get_days_until_birthday
 
 Base = declarative_base()
 
@@ -16,6 +16,7 @@ class Person(Base):
     location = relationship("Location", uselist=False, back_populates="person")
     phone = Column(String)
     cell = Column(String)
+    dob = relationship("Dob", uselist=False, back_populates="person")
 
     def __repr__(self):
         return f'(id:{self.id}, gender:{self.name})'
@@ -45,7 +46,7 @@ class Dob(Base):
     __tablename__ = 'dob'
     id = Column(Integer, primary_key=True)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship("Person", back_populates="location")
+    person = relationship("Person", back_populates="dob")
     date = Column(String)
     age = Column(Integer)
     days_until_birth = Column(Integer)
@@ -87,10 +88,12 @@ location.street = Street(name=get_triple_nested_table_data(0, 'location', 'stree
 dob.date = get_double_nested_table_data(0, "dob", "date")
 dob.age = get_double_nested_table_data(0, "dob", "age")
 dob.days_until_birth = get_days_until_birthday(0, "dob", "date")
+dob.person_id = 1
 
 # To add data
 session.add(person)
 session.add(location)
+session.add(dob)
 
 session.commit()
 
