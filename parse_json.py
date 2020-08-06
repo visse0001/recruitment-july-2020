0ab7b104120008789e32b40012e537846082ff2b
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-06 20:28:24 +0200

    function get_days_until_birthday works

--- parse_json.py
+++ parse_json.py
@@ -48,80 +48,53 @@ def count_persons():
     return len(obj)
 
 
-def is_leap_year(year: int, month, day: int):
-    """
-    Leap year is a year that compiles requirements:
-    - is divisible by 4
-    - is not divisible by 100
-    - is divisable by 400
-    Each leap year has 366 days instead of 365,
-    by extending February to 29 days
-    rather than the common 28.
-    """
-    if (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)) and month == 2 and day == 29:
-        return True
-
-
 def get_days_until_birthday(index: int, dob: str, date: str):
     # create two datetime objects
     now = datetime.now()
     current_year = now.year
+    next_year = current_year + 1
     birthday_str = get_double_nested_table_data(index, dob, date)
     birthday_datetime_obj = datetime.strptime(birthday_str, "%Y-%m-%dT%H:%M:%S.%fZ")
 
     # change birth year to current year
     obj_birthday_with_current_year = birthday_datetime_obj.replace(year=current_year)
 
-    # delta time
-    delta = obj_birthday_with_current_year - now
-
-    delta_days = delta.days
-
-    month = obj_birthday_with_current_year.month
+    # check if bithday month and day is 29 February
     day = obj_birthday_with_current_year.day
-    # check if the delta is a negative number
-    if delta_days < 0:
-        # it means that the birthday was already this year
-
-        # need to look at the next year
-        next_year = int(current_year + 1)
-        obj_birthday_with_current_year.replace(year=next_year)
-
-        # get bithday month and day
-        month = obj_birthday_with_current_year.month
-        day = obj_birthday_with_current_year.day
-
-        # check if this date is in a leap year
-        if is_leap_year(current_year, month, day):
-            # then change day from 29 to 28 and add to delta.days + 1 missing day
-            birthday_with_eariel_day = obj_birthday_with_current_year.replace(day=28)
-            delta = birthday_with_eariel_day - now
-            delta_days = delta.days + 1
-            return delta_days
+    month = obj_birthday_with_current_year.month
+    if day == 29 and month == 2:
+        # change day 29 to 28. Then add 1 day to delta.days
+        new_birthday = obj_birthday_with_current_year.replace(day=28)
 
         # delta
-        delta = obj_birthday_with_current_year - now
-        delta_days = delta.days
-        return delta_days
-    else:
-        # birthday will be in this year
+        delta = new_birthday - now
+        delta_days = delta.days + 1
 
-        # check if this date is in a leap year
-        if is_leap_year(current_year, month, day):
-            # then change day from 29 to 28 and add to delta.days + 1 missing day
-            birthday_with_eariel_day = obj_birthday_with_current_year.replace(day=28)
-
-            # delta
-            delta = now - birthday_with_eariel_day
-            delta_days = delta.days + 1
-            return delta_days
-        else:
-            # delta
-            delta = now - obj_birthday_with_current_year
+        # bithday was in this year. Need to use next year
+        if delta_days < 0:
+            new_birthday = new_birthday.replace(year=next_year)
+            delta = new_birthday - now
             delta_days = delta.days
             return delta_days
 
+        # if birthday will be in this year
+        return delta_days
+
+    # birthday month and day is not 29 February
+    else:
+        delta = obj_birthday_with_current_year - now
+        delta_days = delta.days
+
+        # birthday was in this year. Need to use next year
+        if delta_days < 0:
+            new_birthday = obj_birthday_with_current_year.replace(year=next_year)
+            delta = new_birthday - now
+            delta_days = delta.days
+            return delta_days
+
+        # if birthday will be in this year
+        return delta_days
+
 
 g = get_days_until_birthday(398, "dob", "date")
-# g = get_double_nested_table_data(398, "dob", "date")
 print(g)
