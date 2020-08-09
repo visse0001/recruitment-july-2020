205fd6c59335fd1c507b957b66c8ebd6c081540e
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-09 21:00:07 +0200

    set echo to false

--- argp.py
+++ argp.py
@@ -1,5 +1,6 @@
 import argparse
-from queries import perc_man, perc_women, most_common_cities
+from queries import perc_man, perc_women, average_age_overall, average_age_female, most_common_cities
+
 
 def positive_int(s: str) -> int:
     try:
@@ -12,18 +13,38 @@ def positive_int(s: str) -> int:
 
     return value
 
+
 parser = argparse.ArgumentParser(description='A script that does operations with database data and returns values')
-parser.add_argument('-a', '--average_age',
+parser.add_argument('-p', '--perc',
+                    choices=['man', 'women'],
+                    default='man',
+                    help='Specify percent: man or women. Default: %(man)s.')
+
+parser.add_argument('--average_age',
                     choices=['man', 'women', 'all'],
-                    nargs=1,
-                    default='all',
-                    help='Specify whose average age: all, man or women. Defaulf: %(default)s')
+                    # default='all',
+                    help='Specify average age: man, women, all. Default: %(all)s.')
 
 parser.add_argument('-c', '--most_common_cities',
                     nargs=1,
                     type=positive_int,
-                    help='Specify how many common cities')
+                    help='Specify how many common cities.')
 
 args = parser.parse_args()
 
+if args.perc == 'man':
+    result = perc_man()
+    print(result)
+
+if args.perc == 'women':
+    result = perc_women()
+    print(result)
+
+if args.average_age == 'all':
+    result = average_age_overall()
+    print(result)
+
+if args.average_age == 'female':
+    result = average_age_female()
+    print(result)
 
