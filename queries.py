78fc714121fb0f0c2e8eeddd21e258bc4f7af92d
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-09 17:25:18 +0200

    delete redundant comments

--- queries.py
+++ queries.py
@@ -82,7 +82,6 @@ def is_born_in_date_range():
     end_date = "2010-08-11"
     obj_start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
     obj_end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
-    # obj_end_date returns <class 'datetime.date'> 2020-08-11
 
     list_tuples_birthday_dates = session.query(Dob.date).all()
     list_bithday_dates = [item for t in list_tuples_birthday_dates for item in t]
@@ -109,7 +108,6 @@ def is_born_in_date_range():
             ids_list = [dates_ids[x] for x in birthday_persons if dates_ids.get(x)]
 
     # Find persons names by id
-    print(ids_list)
     list_of_tuples_names = []
     for i in ids_list:
         query = session.query(Name).get(i)
