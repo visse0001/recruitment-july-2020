610626e3c98235dd6e58912fb60f06dd98fd56c2
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-23 20:04:41 +0200

    in func get_data_from_api move one line up

--- api.py
+++ api.py
@@ -7,9 +7,8 @@ def get_data_from_api():
         response = requests.get('https://randomuser.me/api/?results=1000')
         response.raise_for_status()
         json_response = response.json()
+        return json_response
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
         print(f'Other error occurred: {err}')
-
-    return json_response
