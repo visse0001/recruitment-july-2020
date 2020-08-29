573df72b5dc33139543bc103afcd203c86adc0ac
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-08-29 08:55:02 +0200

    Delete get_days_until_birthday function - it was too long.

--- parse_json.py
+++ parse_json.py
@@ -39,48 +39,4 @@ def count_persons():
 
 
 def get_days_until_birthday(index: int, dob: str, date: str):
-    # create two datetime objects
-    now = datetime.now()
-    current_year = now.year
-    next_year = current_year + 1
-    birthday_str = get_double_nested_table_data(index, dob, date)
-    birthday_datetime_obj = datetime.strptime(birthday_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-
-    # change birth year to current year
-    obj_birthday_with_current_year = birthday_datetime_obj.replace(year=current_year)
-
-    # check if bithday month and day is 29 February
-    day = obj_birthday_with_current_year.day
-    month = obj_birthday_with_current_year.month
-    if day == 29 and month == 2:
-        # change day 29 to 28. Then add 1 day to delta.days
-        new_birthday = obj_birthday_with_current_year.replace(day=28)
-
-        delta = new_birthday - now
-        # Add missing 1 day to delta result
-        delta_days = delta.days + 1
-
-        # bithday was in this year. Need to use next year
-        if delta_days < 0:
-            new_birthday = new_birthday.replace(year=next_year)
-            delta = new_birthday - now
-            delta_days = delta.days
-            return delta_days
-
-        # if birthday will be in this year
-        return delta_days
-
-    # birthday month and day is not 29 February
-    else:
-        delta = obj_birthday_with_current_year - now
-        delta_days = delta.days
-
-        # birthday was in this year. Need to use next year
-        if delta_days < 0:
-            new_birthday = obj_birthday_with_current_year.replace(year=next_year)
-            delta = new_birthday - now
-            delta_days = delta.days
-            return delta_days
-
-        # if birthday will be in this year
-        return delta_days
+    pass
