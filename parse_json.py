2e9ffeacfa1a946f96be64f695c6e2ad7d4d3a33
Sandra_Kuczynska <sandra.k@aibusters.com>
2020-12-21 12:46:58 +0100

    Add is_leap_year to get_days_until_birthday method

--- parse_json.py
+++ parse_json.py
@@ -29,11 +29,24 @@ class ParseData:
         return len(obj)
 
     def get_days_until_birthday(self, index: int, dob: str, bday: str):
+        is_leap_year = False
         today = datetime.datetime.today()
         birthday = self.get_double_nested_table_data(index, dob, bday)
         birthday = datetime.datetime.strptime(birthday, "%Y-%m-%dT%H:%M:%S.%fZ")
+        if self.__is_leap_year(birthday):
+            is_leap_year = True
+            birthday = self.__substract_day(birthday)
         birthday.replace(year=today.year)
         if birthday < today:
             birthday = birthday.replace(year=today.year + 1)
         days_to_birthday = abs(birthday - today)
+        if is_leap_year:
+            days_to_birthday.days += 1
         return days_to_birthday.days
+
+    def __is_leap_year(self, birthday):
+        if (birthday.year % 4 == 0) and (birthday.year % 100 != 0):
+            return True
+
+    def __substract_day(self, birthday):
+        return birthday.replace(day=28)
