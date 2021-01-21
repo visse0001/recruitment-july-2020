085263182bfd4b4c400632aaba2238c46fdac6c6
Sandra_Kuczynska <sandra.k@aibusters.com>
2021-01-21 10:36:26 +0100

    Delete redundant imports

--- db_tables.py
+++ db_tables.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
